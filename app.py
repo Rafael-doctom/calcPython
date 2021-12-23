f72f425be8e47129507c658d64dfa2913607c01b
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2021-12-23 02:58:28 -0300

    feat: add details to code

--- app.py
+++ app.py
@@ -4,25 +4,33 @@ numberOne = int(input("Digite um número:"))
 numberTwo = int(input("Digite um 2° números:"))
 
 print("Operações disponíveis: ")
-print("Soma(+)")
-print("Subtração(-)")
-print("Multiplicação(x)")
-print("Divisão(/)")
+print("1) Soma(+)")
+print("2) Subtração(-)")
+print("3) Multiplicação(x)")
+print("4) Divisão(/)")
 
 calcOperation = input("Qual operação deseja realizar:")
 
-if calcOperation == '+':
+if calcOperation == '+' or '1' or 'soma' or 'Soma' or 'SOMA':
     valueTot = numberOne + numberTwo
-    print(valueTot)
-elif calcOperation == '-':
-    print("erro")
-elif calcOperation == 'x':
+    print(f'Resultado:{numberOne} + {numberTwo} = {valueTot}')
+    
+elif calcOperation == '-' or '2' or 'menos' or 'MENOS':
+    valueTot = numberOne - numberTwo
+    print(f'Resultado:{numberOne} - {numberTwo} = {valueTot}')
+    
+elif calcOperation == 'x' or '3' or 'vezes' or 'VEZES':
     valueTot = numberOne * numberTwo
-    print(valueTot)
-elif calcOperation == '/':
+    print(f'Resultado:{numberOne} * {numberTwo} = {valueTot}')
+    
+elif calcOperation == '/' or '4' or 'divisao' or 'divisão' or 'DIVISAO' or 'DIVISÃO':
     valueTot = numberOne / numberTwo
-    print(valueTot)
+    print(f'Resultado:{numberOne} / {numberTwo} = {valueTot}')
+
 elif calcOperation == '':
     print("[ERRO] - talvez você tenha errado em algo...")
 
+else: 
+    print("[OPERAÇÃO INVÁLIDA]")
+    
 print(f'[FIM]- Resultado final foi: {valueTot}')    
