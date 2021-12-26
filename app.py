f25f476a2d390457711b70f190fe118c051ff195
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2021-12-26 01:20:16 -0300

    fix: review errors

--- app.py
+++ app.py
@@ -9,28 +9,24 @@ print("2) Subtração(-)")
 print("3) Multiplicação(x)")
 print("4) Divisão(/)")
 
-calcOperation = input("Qual operação deseja realizar:")
+calcOperation = int(input("Qual operação deseja realizar:"))
+
 
 if calcOperation == '+' or '1' or 'soma' or 'Soma' or 'SOMA':
     valueTot = numberOne + numberTwo
     print(f'Resultado:{numberOne} + {numberTwo} = {valueTot}')
-    
+
 elif calcOperation == '-' or '2' or 'menos' or 'MENOS':
     valueTot = numberOne - numberTwo
     print(f'Resultado:{numberOne} - {numberTwo} = {valueTot}')
-    
+
 elif calcOperation == 'x' or '3' or 'vezes' or 'VEZES':
     valueTot = numberOne * numberTwo
     print(f'Resultado:{numberOne} * {numberTwo} = {valueTot}')
-    
+
 elif calcOperation == '/' or '4' or 'divisao' or 'divisão' or 'DIVISAO' or 'DIVISÃO':
     valueTot = numberOne / numberTwo
     print(f'Resultado:{numberOne} / {numberTwo} = {valueTot}')
 
-elif calcOperation == '':
-    print("[ERRO] - talvez você tenha errado em algo...")
 
-else: 
-    print("[OPERAÇÃO INVÁLIDA]")
-    
-print(f'[FIM]- Resultado final foi: {valueTot}.')    
+print(f'[FIM]- Resultado final foi: {valueTot}.')
